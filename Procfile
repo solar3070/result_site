67f32333e38fa5197d2078f1d06d8b5eb4e7bfed
HyeJun Lee <solar3070@naver.com>
2021-03-15 00:01:11 +0900

    commit message

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn announcemnet.wsgi --log-file -
--- announcement/settings.py
+++ announcement/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'announcement.urls'
@@ -121,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Heroku: Update database configuration from $DATABASE_URL. 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500) 
+DATABASES['default'].update(db_from_env)
